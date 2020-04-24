bdc0c4c7fa4c5b71cdbaae9e68955a96d69d2c59
diogosantosmendes <diogosantosmendes@hotmail.com>
2020-04-24 22:38:20 +0100

    work done

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,39 +1,33 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
-import {Navbar, Nav} from 'react-bootstrap';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavbarComponent from "../components/global/Navbar";
+import PrivateRoute from "../components/global/PrivateRoute";
+import roles from "../configs/roles";
 
-import Home from '../pages/home/Home';
-import About from '../pages/about/About';
-import BookListPage from '../pages/book/List';
-import BookDetailsPage from '../pages/book/Details';
+import LoginPage from "../pages/auth/Login";
+import RegisterPage from "../pages/auth/Register";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import BookListPage from "../pages/book/List";
+import BookDetailsPage from "../pages/book/Details";
 
 export default class RouterComponent extends React.Component {
-  render () {
+  render() {
     return (
       <Router>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <NavLink to="*" component={Nav.Link}>Home</NavLink>
-              <NavLink to="/about" component={Nav.Link}>Lesson 3</NavLink>
-              <NavLink to="/book/list" component={Nav.Link}>
-                Lesson 4 - Books
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
+        <NavbarComponent />
         <Switch>
-
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/login" component={LoginPage} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/book/list" component={BookListPage} />
-          <Route exact path="/book/details/:id" component={BookDetailsPage} />
-          <Route exact path="*" component={Home} />
+          <PrivateRoute roles={[roles.Boss, roles.Servant]} exact path="/book/list" component={BookListPage} />
+          <PrivateRoute
+            roles={[roles.Boss, roles.Servant]}
+            exact
+            path="/book/details/:id"
+            component={BookDetailsPage}
+          />
+          <Route path="*" component={Home} />
         </Switch>
       </Router>
     );
